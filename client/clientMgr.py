9876fe68753770627827634b6597b6137e3bd98e
811417477@qq.com <811417477@qq.com>
2019-09-17 16:18:54 +0800

    添加模拟客户端

---
+++ client/clientMgr.py
@@ -0,0 +1,34 @@
+from urllib import request, parse
+import json
+import requests
+
+# 电脑地址 http://127.0.0.1:10001
+# 域名地址 http://thiswen.cn:10001
+
+# getValue地址
+get_value_path = "http://127.0.0.1:10001/getVal"
+# getValue地址
+set_value_path = "http://127.0.0.1:10001/setVal"
+
+# 模拟客户端管理器
+class ClientMgr(object):
+    def __init__(self, *args):
+        pass
+        
+    # 发送get请求
+    def send_get(self, key):
+        send_str = "%s?key=%s"%(get_value_path, key)
+        ret = request.Request(send_str)
+        response = request.urlopen(ret)
+        # print(response.read().decode('utf-8'))
+    
+    # 发送post请求
+    def send_post(self, key, val):
+        headers = {'content-type':'application/json'}
+        data = {
+            'key': key,
+            'val': val
+        }
+        response = requests.post(set_value_path, data=json.dumps(data),headers=headers)
+        # print(response.text)
+
--- clientMain.py
+++ clientMain.py
@@ -1,24 +1,16 @@
-import urllib
-from urllib import request, parse
-import json
-import requests
+
+import time
+
+import client.clientMgr as clientMgrMd
 
 # 模拟客户端请求
 
-# 电脑地址 http://127.0.0.1:10001
-# 域名地址 http://thiswen.cn:10001
+clientMgr = clientMgrMd.ClientMgr()
 
-# get
-ret = request.Request('http://127.0.0.1:10001/getVal?key=a')
-response = request.urlopen(ret)
-print(response.read().decode('utf-8'))
+clientMgr.send_post("a","b")
 
-# post
-headers = {'content-type':'application/json'}
-url="http://127.0.0.1:10001/setVal"  
-data = {
-    'key': 'a',
-    'val': '1'
-}
-response = requests.post(url, data=json.dumps(data),headers=headers)
-print(response.text)
+now = time.time()
+for item in range(1000):
+    clientMgr.send_get("a")
+end = time.time()
+print(end - now)
